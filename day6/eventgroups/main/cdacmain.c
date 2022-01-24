21e5c90ea9afee715227a5ab53cb34dbd007a38b
SREERAMULA-SAIKUMAR <sreeramulasaikumar.com>
2022-01-24 15:05:14 +0530

    eventgroups program added

---
+++ day6/eventgroups/main/cdacmain.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<freertos/FreeRTOS.h>
+#include<freertos/task.h>
+#include<freertos/timers.h>
+#include<freertos/event_groups.h>
+
+TaskHandle_t temp_handle;
+TaskHandle_t pres_handle;
+TaskHandle_t calb_handle;
+
+EventGroupHandle_t sensor_eg;
+
+const uint32_t temp_event_bit=(1<<0);
+const uint32_t pres_event_bit=(1<<1);
+const uint32_t all_bits=(temp_event_bit | pres_event_bit);
+
+void TemperatureTask(void *pv)
+{
+    while (1)
+    {
+        printf("Temperature Task\n");
+        xEventGroupSetBits(sensor_eg, temp_event_bit);
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+    }
+    
+}
+
+void PressureTask(void *pv)
+{
+    while (1)
+    {
+        printf("Pressure Task\n");
+        xEventGroupSetBits(sensor_eg, pres_event_bit);
+        vTaskDelay(2000/portTICK_PERIOD_MS);
+    }
+}
+
+void CalibrationTask(void *pv)
+{
+    uint32_t result;
+    while (1)
+    {
+        printf("Calibration Task\n");
+        result=xEventGroupWaitBits(sensor_eg, all_bits, pdTRUE, pdTRUE, pdMS_TO_TICKS(5000));
+        printf("Result: %d\n", result);
+    }
+    vTaskDelete(NULL);
+}
+
+void app_main()
+{
+    sensor_eg=xEventGroupCreate();
+    xTaskCreate(TemperatureTask, "TemperatureTask", 2048, NULL, 5, &temp_handle);
+    xTaskCreate(PressureTask, "PressureTask", 2048, NULL, 5, &pres_handle);
+    xTaskCreate(CalibrationTask, "CalibrationTask", 2048, NULL, 5, &calb_handle);
+
+    while (1)
+    {
+        printf("Main Task\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    
+}
