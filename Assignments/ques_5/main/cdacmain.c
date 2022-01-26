dd2bb26783d5af30137d4d836a1ad0099c1afc72
SREERAMULA-SAIKUMAR <sreeramulasaikumar.com>
2022-01-26 20:37:50 +0530

    question 5 program added

--- Assignments/ques_5/main/cdacmain.c
+++ Assignments/ques_5/main/cdacmain.c
@@ -23,12 +23,12 @@ void BuzzerTask(void *pv)
 void app_main()
 {
     BaseType_t result1, result2;
-    result1=xTaskCreate(SensorTask,"SensorTask",1024,NULL,20,NULL);
+    result1=xTaskCreate(SensorTask,"SensorTask",2048,NULL,20,NULL);
     if(result1==pdPASS)
     {
         printf("Sensor Task Created\n");
     }
-    result2=xTaskCreate(BuzzerTask,"BuzzerTask",1024,NULL,10,NULL);
+    result2=xTaskCreate(BuzzerTask,"BuzzerTask",2048,NULL,10,NULL);
     if(result2==pdPASS)
     {
         printf("Buzzer Task Created\n");
