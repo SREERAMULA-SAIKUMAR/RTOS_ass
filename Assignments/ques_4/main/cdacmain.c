90722dcbc01b1be7eeff7990912a5478203ca1da
SREERAMULA-SAIKUMAR <sreeramulasaikumar.com>
2022-01-26 20:21:06 +0530

    question 4 program added

---
+++ Assignments/ques_4/main/cdacmain.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+void SensorTask(void *pv)
+{
+    while(1)
+    {
+        printf("Inside Sensor Task\n");
+        vTaskDelay(1000/ portTICK_PERIOD_MS);
+    }
+}
+
+void BuzzerTask(void *pv)
+{
+    while(1)
+    {
+        printf("Inside Buzzer Task\n"); 
+        vTaskDelay(1000/ portTICK_PERIOD_MS);
+    }
+}
+
+void app_main()
+{
+    BaseType_t result1,result2;
+    result1=xTaskCreate(SensorTask,"SensorTask",2048,NULL,10,NULL);
+    if(result1==pdPASS)
+    {
+        printf("Sensor Task Created\n");
+    }
+
+    result2=xTaskCreate(BuzzerTask,"BuzzerTask",2048,NULL,20,NULL);
+    if(result2==pdPASS)
+    {
+        printf("Buzzer Task Created\n");
+    }
+}
